bf86185fb25dc31a197df08505a3e2afc74182d2
Gjorgi Krenkov <gjorgi.krenkov@codechem.com>
2024-12-07 11:01:14 +0100

    Day7

---
+++ Day7.js
@@ -0,0 +1,20 @@
+const fs = require('node:fs');
+
+const data = fs.readFileSync('input.txt').toString();
+
+const rows = data.split("\r\n");
+
+let totalSum = 0;
+rows.forEach((row) => {
+    const [neededRaw, opRaw] = row.split(': ')
+    const needed = Number.parseInt(neededRaw);
+    op = opRaw.split(' ').map((c) => Number.parseInt(c));
+    let resultSet = [op[0] + op[1], op[0] * op[1], Number.parseInt(`${op[0]}${op[1]}`)];
+    for(let i = 2; i < op.length; i++)
+    {
+        resultSet = resultSet.flatMap((el) => ([el + op[i], el * op[i],Number.parseInt(`${el}${op[i]}`)]));
+        resultSet = [... new Set(resultSet)];
+    }
+    if(resultSet.includes(needed)) totalSum += needed;
+})
+console.log(totalSum);
