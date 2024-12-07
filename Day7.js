a100d23810931ab4a6cd4d22e54af750d5773d70
Gjorgi Krenkov <gjorgi.krenkov@codechem.com>
2024-12-07 11:02:39 +0100

    Day7 + CtrlShiftF

--- Day7.js
+++ Day7.js
@@ -10,11 +10,10 @@ rows.forEach((row) => {
     const needed = Number.parseInt(neededRaw);
     op = opRaw.split(' ').map((c) => Number.parseInt(c));
     let resultSet = [op[0] + op[1], op[0] * op[1], Number.parseInt(`${op[0]}${op[1]}`)];
-    for(let i = 2; i < op.length; i++)
-    {
-        resultSet = resultSet.flatMap((el) => ([el + op[i], el * op[i],Number.parseInt(`${el}${op[i]}`)]));
+    for (let i = 2; i < op.length; i++) {
+        resultSet = resultSet.flatMap((el) => ([el + op[i], el * op[i], Number.parseInt(`${el}${op[i]}`)]));
         resultSet = [... new Set(resultSet)];
     }
-    if(resultSet.includes(needed)) totalSum += needed;
+    if (resultSet.includes(needed)) totalSum += needed;
 })
 console.log(totalSum);
