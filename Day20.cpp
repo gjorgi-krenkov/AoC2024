896717544e8da106cd58cf33bc0371614d53358e
Gjorgi Krenkov <gjorgi.krenkov@codechem.com>
2024-12-20 08:27:04 +0100

    Day20 p2

--- Day20.cpp
+++ Day20.cpp
@@ -72,7 +72,7 @@ int main()
         }
     }
     int orgRes = 0;
-    bool visited[X][Y];
+
     reachMap[start.first][start.second] = 0;
     dijkstra.push({{0, start}, "right"});
     while (dijkstra.size())
@@ -86,13 +86,11 @@ int main()
         {
             pair<int, int> delta = getDelta(dir);
 
-            visited[coords.first][coords.second] = true;
             if (
                 isValid(coords.first + delta.first, coords.second + delta.second) && (rows[coords.first + delta.first][coords.second + delta.second] == '.' || rows[coords.first + delta.first][coords.second + delta.second] == 'E') && reachMap[coords.first + delta.first][coords.second + delta.second] > val + 1)
             {
                 dijkstra.push({{val + 1, {coords.first + delta.first, coords.second + delta.second}}, dir});
                 reachMap[coords.first + delta.first][coords.second + delta.second] = val + 1;
-                visited[coords.first + delta.first][coords.second + delta.second] = false;
             }
             dir = getClockDir(dir);
         }
@@ -102,51 +100,46 @@ int main()
         }
     }
     orgRes = reachMap[finish.first][finish.second];
-    cout << orgRes;
     int score = 0;
-    for (int i = 1; i < X - 1; i++)
+    for (int i = 0; i < X; i++)
     {
-        for (int j = 1; j < Y - 1; j++)
+        for (int j = 0; j < Y; j++)
         {
-            cout << i << " " << j << " " << X << " " << Y << endl;
-            if (orgMp[i][j] != '#')
+            if (orgMp[i][j] != '.' && orgMp[i][j] != 'S' && orgMp[i][j] != 'E')
                 continue;
-            rows[i][j] = '.';
-            for (int l = 0; l < X; l++)
-            {
-                for (int v = 0; v < Y; v++)
-                    reachMap[l][v] = 99999999;
-            }
-            while (dijkstra.size())
-                dijkstra.pop();
+            int foundCheat = 0;
             bool visited[X][Y];
-            reachMap[start.first][start.second] = 0;
-            dijkstra.push({{0, start}, "right"});
-            while (dijkstra.size())
+            for (int q = 0; q < X; q++)
+                for (int z = 0; z < Y; z++)
+                    visited[q][z] = false;
+            for (int l = 0; l <= 20; l++)
             {
-                auto per = dijkstra.top();
-                dijkstra.pop();
-                int val = per.first.first;
-                pair<int, int> coords = per.first.second;
-                string dir = per.second;
-                for (int i = 0; i < 4; i++)
+                for (int r = 0; r <= 20; r++)
                 {
-                    pair<int, int> delta = getDelta(dir);
-
-                    visited[coords.first][coords.second] = true;
-                    if (
-                        isValid(coords.first + delta.first, coords.second + delta.second) && (rows[coords.first + delta.first][coords.second + delta.second] == '.' || rows[coords.first + delta.first][coords.second + delta.second] == 'E') && reachMap[coords.first + delta.first][coords.second + delta.second] > val + 1)
+                    if (l + r > 20)
+                        continue; // original cost - cost to current + num of hor ver moves >= 50
+                    vector<pair<int, int>> dirs = {{0, 1}, {1, 0}, {-1, 0}, {0, -1}, {1, 1}, {-1, 1}, {1, -1}, {-1, -1}};
+                    for (auto dir : dirs)
                     {
-                        dijkstra.push({{val + 1, {coords.first + delta.first, coords.second + delta.second}}, dir});
-                        reachMap[coords.first + delta.first][coords.second + delta.second] = val + 1;
-                        visited[coords.first + delta.first][coords.second + delta.second] = false;
+                        int fin = reachMap[finish.first][finish.second];
+                        int tar = reachMap[i + l * dir.first][j + r * dir.second];
+                        int cur = reachMap[i][j];
+
+                        if (
+                            isValid(i + l * dir.first, j + r * dir.second)                 // valid .
+                            && !visited[i + l * dir.first][j + r * dir.second]             // we haven't reached from here yet
+                            && reachMap[i + l * dir.first][j + r * dir.second] != 99999999 // we don't end on #
+                            && (cur - (l + r) - 100) >= tar                                // from here + 20 - (50 to save) reaches faster then tar
+                        )
+                        {
+
+                            visited[i + l * dir.first][j + r * dir.second] = true; // mark the field as visited from this pos
+                            foundCheat++;
+                        }
                     }
-                    dir = getClockDir(dir);
                 }
             }
-            if (orgRes - reachMap[finish.first][finish.second] >= 100)
-                score++;
-            rows[i][j] = '#';
+            score += foundCheat;
         }
     }
     cout << score;
